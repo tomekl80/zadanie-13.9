387f40c99a5d3a5f4589c0ff4cf44ef92339b4eb
Tomasz Lis <tomasz.lis@o2.pl>
2018-10-21 16:31:30 +0200

    Fix problem witch symbol ) in line 21 handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -16,11 +16,11 @@ exports.upload = function (request, response) {
 }
 exports.welcome = function (request, response) {
 	console.log('Rozpoczynam obsługę żądania welcome.');
-	fs.readFile('templates/start.html'), function (err, html) {
-		response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+	fs.readFile('templates/start.html', function (err, html) {
+		response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 		response.write(html);
 		response.end();
-	};
+	});
 }
 exports.error = function (request, response) {
 	console.log('Nie wiem co robić.');
